06d23fedc3279283190dffa455c482e7d40df69d
David Timm <timmmeister@gmail.com>
2016-08-01 16:09:54 -0600

    Run agent update.

--- main.py
+++ main.py
@@ -24,12 +24,19 @@ def main():
 
         # Run 200 time steps
         for t in range(200):
-            env.render()
+            # Save the previous state.
+            prev_state = observation
+
+            if i_episode % 100 == 0:
+                env.render()
 
             next_action = agent.get_action(observation)
 
             observation, reward, done, info = env.step(next_action)
             score += reward
+
+            agent.update(prev_state, next_action, reward, observation)
+
             if done or t == 199:
                 print score
                 break
